fdbb2731fda9b3b05034c68b5ff2986cfab46915
ESGI5SRC <fred.hyoudro@gmail.com>
2024-07-13 20:52:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "webapp-esgi-proj4"
+  name     = "webapp-esgi-proj5"
   location = "East US 2"
 }
 
@@ -25,7 +25,7 @@ resource "azurerm_service_plan" "main" {
 }
 
 resource "azurerm_linux_web_app" "app_service" {
-  name                = "webappEsgi-v5"
+  name                = "webappEsgi-v6"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id = azurerm_service_plan.main.id
@@ -38,7 +38,7 @@ resource "azurerm_linux_web_app" "app_service" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "esgiwebapp4" 
+  name                     = "esgiwebapp5" 
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -60,12 +60,6 @@ resource "azurerm_storage_blob" "example" {
   storage_account_name   = azurerm_storage_account.storage.name
   storage_container_name = azurerm_storage_container.container.name
   type                   = "Block"
-}
-
-resource "azurerm_app_service_source_control" "source_control" {
-  app_id                 = azurerm_linux_web_app.app_service.id
-  repo_url               = "https://github.com/Nicolas7D/Webapp-ESGI.git"
-  branch                 = "main"
 }
 
 resource "azurerm_mssql_server" "sqlserver" {
@@ -318,3 +312,4 @@ resource "azurerm_monitor_autoscale_setting" "scaling" {
     } 
   }
 }
+
