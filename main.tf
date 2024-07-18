6271c402946a390847bdde28ee2b5b0d32fd2677
ESGI5SRC <fred.hyoudro@gmail.com>
2024-07-18 12:18:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "webapp-esgi-proj8"
+  name     = "webapp-esgi-proj9"
   location = "East US 2"
 }
 
@@ -25,7 +25,7 @@ resource "azurerm_service_plan" "main" {
 }
 
 resource "azurerm_linux_web_app" "app_service" {
-  name                = "webappEsgi-v9"
+  name                = "webappEsgi-v10"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id = azurerm_service_plan.main.id
@@ -38,7 +38,7 @@ resource "azurerm_linux_web_app" "app_service" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "esgiwebapp8" 
+  name                     = "esgiwebapp9" 
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
